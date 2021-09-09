b9bea4ab94d21e2c7949a1fadcc70bb1eaaa65a3
“nitrolo” <nitrolo225@gmail.com>
2021-09-09 11:59:02 +0530

    added the schema

---
+++ api/src/index.js
@@ -0,0 +1,65 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { DB_URI, DB_NAME } = process.env;
+
+const typeDefs = gql`
+  type Query {
+    myTaskLists: [TaskList!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    avatar: String
+  }
+
+  type TaskList {
+    id: ID!
+    createdAt: String!
+    title: String!
+    progress: Float!
+    users: [User!]!
+    todos: [ToDo!]!
+  }
+
+  type ToDo {
+    id: ID!
+    content: String
+    isCompleted: Boolean!
+    taskList: TaskList
+  }
+`;
+
+const resolvers = {
+  Query: {
+    myTaskLists: () => [],
+  },
+};
+
+const start = async () => {
+  const client = new MongoClient(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const db = client.db(DB_NAME);
+
+  const context = {
+    db,
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+  // definition and your set of resolvers.
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+};
+
+start();
